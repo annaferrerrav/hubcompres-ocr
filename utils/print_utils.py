ab17c97ed9d1bec56f15fbeee57f2b4abfe804c1
annaferrerrav <aferrerr@clinic.cat>
2025-11-24 14:24:31 +0100

    Add files via upload

---
+++ utils/print_utils.py
@@ -0,0 +1,4 @@
+def strRed(skk): return "\033[91m{}\033[00m" .format(skk)
+def strBlue(skk): return "\033[96m{}\033[00m" .format(skk)
+def strYellow(skk): return "\033[93m{}\033[00m" .format(skk)
+def strGreen(skk): return "\033[92m{}\033[00m".format(skk)
